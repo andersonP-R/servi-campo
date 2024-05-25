ebe17ea8667765011655c2b7a084929ee1bf4666
Anderson Rivera <anderson.pr.dev@gmail.com>
2024-05-25 17:13:54 -0500

    first commit

---
+++ src/helpers/selector.ts
@@ -0,0 +1,6 @@
+/**
+ * Retrieves the DOM element passed by param
+ * @param el DOM element
+ * @returns DOM element
+ */
+export const $ = (el: string) => document.querySelector(el);
---
+++ src/utils/UI_CONST.ts
@@ -0,0 +1,7 @@
+/**
+ * Colors palette
+ */
+export const COLORS = {
+  white: "#eee",
+  black: "#000",
+};
