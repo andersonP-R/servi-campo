85294435a0f9d8288ba6bb5dc63f6bd3d03e9d11
Anderson Rivera <anderson.pr.dev@gmail.com>
2024-05-31 01:06:06 -0500

    color palette added

--- src/utils/UI_CONST.ts
+++ src/utils/UI_CONST.ts
@@ -2,6 +2,11 @@
  * Colors palette
  */
 export const COLORS = {
-  white: "#eee",
+  dark_green: "#162f12",
+  dark_green_soft: "#2e4a20",
+  green: "#4c6d32",
+  light_green: "#7c9e47",
+  brigth_green: "#bcdc60",
+  white: "#fff",
   black: "#000",
 };
